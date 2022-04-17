a3c0d8357ed73bb4ad789b49498e03b032512c27
fikretgit <silvermail1631@gmail.com>
2022-04-17 22:13:25 +0300

    Phonebook Application added

---
+++ Terraform/Project_2-Phonebook Application/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.10.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "4.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+provider "github" {
+  token = "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"
+}
