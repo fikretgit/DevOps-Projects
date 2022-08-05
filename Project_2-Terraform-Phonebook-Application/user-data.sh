650337e3909992529598ef248221486e3a7028a6
fikretgit <silvermail1631@gmail.com>
2022-08-05 13:37:59 +0300

    Readme updated

--- Project_2-Terraform-Phonebook-Application/user-data.sh
+++ Project_2-Terraform-Phonebook-Application/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="xxxxxxxxxxxxxxxxxxxxxxxx"   # type your token here
+TOKEN="xxxxxxxxxxxxxxxxxxxxxxxx"  # type your token here
 cd /home/ec2-user && git clone https://$TOKEN@github.com/fikretgit/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
